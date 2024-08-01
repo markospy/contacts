1a9acb62159f3ebc0874be426976cebb42eb0e9f
Marcos Antonio Avila <marco.developer@outlook.com>
2024-07-31 22:27:49 -0400

    "Removed Vite and React logos, updated CSS styles, and refactored App component to use React Router."

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World!</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
