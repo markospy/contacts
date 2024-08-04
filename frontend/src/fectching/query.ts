0a30b13c8da1dce7196e43619cadebf16992f0f2
Marcos Antonio Avila <marco.developer@outlook.com>
2024-08-04 02:05:48 -0400

    "Added React Query hook to Contacts component"

--- frontend/src/fectching/query.ts
+++ frontend/src/fectching/query.ts
@@ -1,1 +1,8 @@
+import { useQuery  } from "@tanstack/react-query";
+import { getContacts } from "./api";
 
+
+export const Contacts = () => useQuery({ 
+	queryKey: ['contacts'],
+	queryFn: getContacts
+});
