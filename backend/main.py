9ef6af5b8dd820606ae1aa0a7aa8a50732900a90
Marcos Antonio Avila <marco.developer@outlook.com>
2024-08-04 21:13:45 -0400

    "Renamed API endpoints from '/contact' to '/contacts' and rearranged imports"

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,3 @@
-from fastapi import FastAPI
-
 from crud import (
     create_contact,
     delete_contact,
@@ -7,12 +5,13 @@ from crud import (
     get_contact_by_id,
     update_contact,
 )
+from fastapi import FastAPI
 from schemas import Contact, ContactOut
 
 app = FastAPI()
 
 
-@app.get("/contact", status_code=200)
+@app.get("/contacts", status_code=200)
 async def get_all(skip: None | int = None, limit: None | int = None):
     """Obtiene una lista de todos los contactos almacenados en la base de datos."""
     count, contacts_all = get_all_contacts(skip, limit)
@@ -22,14 +21,14 @@ async def get_all(skip: None | int = None, limit: None | int = None):
     return {"count": count, "contacts": contacts}
 
 
-@app.get("/contact/{id}", status_code=200, response_model=ContactOut)
+@app.get("/contacts/{id}", status_code=200, response_model=ContactOut)
 async def get_one(id: str):
     """Obtiene un contacto almacenado en la base de datos por su ID."""
     contact = get_contact_by_id(id)
     return ContactOut(**contact)
 
 
-@app.post("/contact", status_code=201, response_model=ContactOut)
+@app.post("/contacts", status_code=201, response_model=ContactOut)
 async def create(contact: Contact) -> str:
     """Obtiene un contacto almacenado en la base de datos por su ID."""
     id = create_contact(contact.model_dump())
@@ -37,7 +36,7 @@ async def create(contact: Contact) -> str:
     return ContactOut(**contact_created)
 
 
-@app.put("/contact/{id}", status_code=200, response_model=ContactOut)
+@app.put("/contacts/{id}", status_code=200, response_model=ContactOut)
 async def update(id: str, contact: Contact):
     """Actualiza un contacto almacenado en la base de datos por su ID."""
     update_contact(id, contact.model_dump())
@@ -45,7 +44,7 @@ async def update(id: str, contact: Contact):
     return ContactOut(**contact)
 
 
-@app.delete("/contact/{id}", status_code=204)
+@app.delete("/contacts/{id}", status_code=204)
 async def delete(id: str):
     """Actualiza un contacto almacenado en la base de datos por su ID."""
     delete_contact(id)
