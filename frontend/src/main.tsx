976fe8893488df32fee281fb301ae1422bc9ea1c
Marcos Avila <marcos.seocuba@gmail.com>
2024-08-09 10:59:28 -0400

    "Added React Query client and wrapped RouterProvider with QueryClientProvider"

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css'
 import Root, { loader as rootLoader } from './routes/root'
 import ErrorPage from './error-page'
 import Contact from './routes/contact'
+
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    loader: rootLoader,
+    loader: async () => rootLoader(),
     children: [{
       path: 'contacts/:contactId',
       element: <Contact />,
@@ -21,6 +24,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
