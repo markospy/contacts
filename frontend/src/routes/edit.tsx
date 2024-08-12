67d56afd18c50b1a5ec3796faf2debd2aa437215
Marcos Avila <marcos.seocuba@gmail.com>
2024-08-11 23:39:33 -0400

    Add Edit route.

---
+++ frontend/src/routes/edit.tsx
@@ -0,0 +1,60 @@
+import { Form, useLoaderData } from "react-router-dom";
+import { ContactsOut } from '../types/conctact.ts'
+
+
+export default function EditContact() {
+  const contact = useLoaderData() as ContactsOut;
+
+  return (
+    <Form method="post" id="contact-form">
+      <p>
+        <span>Name</span>
+        <input
+          placeholder="First"
+          aria-label="First name"
+          type="text"
+          name="first"
+          defaultValue={contact?.first_name}
+        />
+        <input
+          placeholder="Last"
+          aria-label="Last name"
+          type="text"
+          name="last"
+          defaultValue={contact?.last_name}
+        />
+      </p>
+      <label>
+        <span>Twitter</span>
+        <input
+          type="text"
+          name="twitter"
+          placeholder="@jack"
+          defaultValue={contact?.twitter}
+        />
+      </label>
+      <label>
+        <span>Avatar URL</span>
+        <input
+          placeholder="https://example.com/avatar.jpg"
+          aria-label="Avatar URL"
+          type="text"
+          name="avatar"
+          defaultValue={contact?._id}
+        />
+      </label>
+      <label>
+        <span>Notes</span>
+        <textarea
+          name="notes"
+          defaultValue={contact?.description}
+          rows={6}
+        />
+      </label>
+      <p>
+        <button type="submit">Save</button>
+        <button type="button">Cancel</button>
+      </p>
+    </Form>
+  );
+}
