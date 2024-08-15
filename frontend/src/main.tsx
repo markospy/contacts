2fe292fc90f81f846aa66ec9fcefe5b9b23413f8
Marcos Avila <marcos.seocuba@gmail.com>
2024-08-15 00:04:09 -0400

    "Updated main.tsx: refactored QueryClient, added new routes and actions, and modified existing route configurations"

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,37 +5,44 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
-import { Contact, loader as conctactLoader } from './routes/contact'
+import Contact, { loader as conctactLoader, action as contactAction } from './routes/contact'
 import EditContact, {action as editAction} from "./routes/edit";
+import { action as destroyAction} from './routes/destroy';
+import Index from './routes/index';
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 10,
-    },
-  },
-})
+export const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    loader: () => rootLoader(queryClient),
+    loader: rootLoader(queryClient),
     action: () => rootAction(queryClient),
     children: [
       {
-      path: 'contact/:contactId',
-      element: <Contact />,
-      loader: conctactLoader(queryClient),
-    },
-    {
-      path: "contact/:contactId/edit",
-      element: <EditContact />,
-      loader: conctactLoader(queryClient),
-      action: editAction,
-    },
-  ],
+        errorElement: <ErrorPage />,
+        children: [
+          {index: true, element: <Index />},
+          {
+          path: 'contact/:contactId',
+          element: <Contact />,
+          loader: conctactLoader(queryClient),
+          action: contactAction(queryClient),
+          },
+          {
+            path: "contact/:contactId/edit",
+            element: <EditContact />,
+            loader: conctactLoader(queryClient),
+            action: editAction(queryClient),
+          },
+          {
+            path: "contact/:contactId/destroy",
+            action: destroyAction(queryClient),
+          },
+        ]
+      }
+    ],
   }
 ])
 
