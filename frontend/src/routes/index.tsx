35650e5796a1da31afb72678423fdbf333eb607d
Marcos Avila <marcos.seocuba@gmail.com>
2024-08-21 17:23:03 -0400

    "Update Index component to display new text and image instead of React Router demo"

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,12 @@
 export default function Index() {
     return (
-      <p id="zero-state">
-        This is a demo for React Router.
-        <br />
-        Check out{" "}
-        <a href="https://reactrouter.com">
-          the docs at reactrouter.com
-        </a>
-        .
+      <p className="text-gray-700 font-semibold text-lg md:text-3xl text-center">
+        Your contacts in one place.
+        <img 
+          src="https://i.pinimg.com/736x/68/5c/ce/685cceffa93afa89416c4345481bf834.jpg"
+          alt="contacts"
+          className="w-96 m-auto"
+        />
       </p>
     );
   }
