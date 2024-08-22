d2243e704709dd28bbff6bac036512cb69712960
Marcos Avila <marcos.seocuba@gmail.com>
2024-08-21 21:46:04 -0400

    "Updated CSS and JSX code in index.css, contact.tsx, and root.tsx files"

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -56,7 +56,7 @@ export default function Root() {
     return (
       <>
         <div id="sidebar">
-          <h1>React Router Contacts</h1>
+          <h1>Created by markospy</h1>
           <div>
             <Form id="search-form" role="search" >
               <input
