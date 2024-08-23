fd22581a9b75ca089760e0eae1c1a24ade736bf9
Marcos Avila <marcos.seocuba@gmail.com>
2024-08-23 12:32:59 -0400

    "Updated image source in Index component from external URL to local '/image_home.jpg'"

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,7 +3,7 @@ export default function Index() {
       <p className="text-gray-700 font-semibold text-lg md:text-3xl text-center">
         Your contacts in one place.
         <img 
-          src="https://i.pinimg.com/736x/68/5c/ce/685cceffa93afa89416c4345481bf834.jpg"
+          src="/image_home.jpg"
           alt="contacts"
           className="w-96 m-auto"
         />
